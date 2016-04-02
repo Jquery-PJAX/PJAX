c9b3b2cd1fac2ca692bc065c71ff5984caa77de8
K Stanton <KMStanton@users.noreply.github.com>
2016-04-02 16:28:26 -0500

    Create example.php

---
+++ php/example.php
@@ -0,0 +1,8 @@
+<?php
+$link_text = '';//where the visual text goes
+$div = '';//targeted div
+$href = '';//where the data is going
+$url = '';//data being transmitted
+$loads = '';//what runs when the new content is loaded
+
+make_link($link_text, $div, $href, $url, $loads);
