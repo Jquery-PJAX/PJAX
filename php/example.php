5f80502276ac70aef4f4b3fcc77e8a9c77238506
K Stanton <KMStanton@users.noreply.github.com>
2016-04-04 15:31:34 -0500

    Update example.php

--- php/example.php
+++ php/example.php
@@ -1,8 +1,8 @@
 <?php
-$link_text = '';//where the visual text goes
-$div = '';//targeted div
-$href = '';//where the data is going
-$url = '';//data being transmitted
-$loads = '';//what runs when the new content is loaded
+$link_text = 'displayed text';//where the visual text goes
+$div = 'targeted id';//targeted div
+$href = 'base.html';//where the data is going
+$url = 'variable1=1&variable2=2';//data being transmitted
+$loads = 'dialog();';//what runs when the new content is loaded
 
 make_link($link_text, $div, $href, $url, $loads);
