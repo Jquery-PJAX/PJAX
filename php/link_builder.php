bee61fe5ef2d0537e21165d1fcc2c6ec583128ec
K Stanton <KMStanton@users.noreply.github.com>
2016-04-04 16:20:23 -0500

    Update link_builder.php

--- php/link_builder.php
+++ php/link_builder.php
@@ -1,4 +1,6 @@
 <?php
 function make_link($link_text, $div, $href, $url, $loads){
-    echo"<a href='?$url' class='box_link' onclick='return false' onmousedown='swapContent(\"$href\", \"$url\", $div, $loads)'>$link_text</a>";
+    echo"<a href='?$url' onclick='return false;' onmousedown='swapContent(\"$href\", \"$url\", $div, \"$loads\")'>$link_text</a>
+
+";
 }
