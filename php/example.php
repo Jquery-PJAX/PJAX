e0f8b1856d9fb0abaa438d7222de3a534a0ecbb0
K Stanton <KMStanton@users.noreply.github.com>
2016-04-04 15:32:33 -0500

    Update example.php

--- php/example.php
+++ php/example.php
@@ -1,6 +1,6 @@
 <?php
 $link_text = 'displayed text';//where the visual text goes
-$div = 'targeted id';//targeted div
+$div = 'targetedID';//targeted div
 $href = 'base.html';//where the data is going
 $url = 'variable1=1&variable2=2';//data being transmitted
 $loads = 'dialog();';//what runs when the new content is loaded
