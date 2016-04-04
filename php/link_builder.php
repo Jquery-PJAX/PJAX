d46e201139edde29677a163fec1e148aebe6c033
K Stanton <KMStanton@users.noreply.github.com>
2016-04-04 16:49:36 -0500

    Update link_builder.php

--- php/link_builder.php
+++ php/link_builder.php
@@ -1,5 +1,5 @@
 <?php
-function make_link($link_text, $div, $href, $url, $loads){
+function make_link($link_text, $div, $href, $url, $loads=''){
     echo"<a href='?$url' onclick='return false;' onmousedown='swapContent(\"$href\", \"$url\", $div, \"$loads\")'>$link_text</a>
 
 ";
