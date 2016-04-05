e4ea0a984fc6bef7f11085ddc7bc91ffd0ce7a7c
K Stanton <KMStanton@users.noreply.github.com>
2016-04-04 23:21:51 -0500

    Update example.js

--- javascript/example.js
+++ javascript/example.js
@@ -1,5 +1,5 @@
 $(document).on("click",".some",function(){
-    swapContent("some.php",{foo:12,bar:32},"#main",function(){
+    swapContent("some.php",{'foo=12&bar=32'},"#main",function(){
         console.log(this,arguments)
     })
 })
